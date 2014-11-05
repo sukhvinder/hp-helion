f9887e55ee47076cf31f67cb1f4bce8f9368e451
root <root@localhost.localdomain>
2014-11-05 06:19:00 -0800

    Added yum cookbook

--- chef-repo/cookbooks/tomcat/recipes/hpcloud.rb
+++ chef-repo/cookbooks/tomcat/recipes/hpcloud.rb
@@ -2,7 +2,7 @@ name "java-app-server"
 run_list("recipe[tomcat]")
 override_attributes(
   'tomcat' => {
-    'java_options' => "${JAVA_OPTS} -Xmx128M -Djava.awt.headless=true"
-    'base_version' => "tomcat*"
+    'java_options' => "${JAVA_OPTS} -Xmx128M -Djava.awt.headless=true",
+    'base_version' => "7"
   }
 )
